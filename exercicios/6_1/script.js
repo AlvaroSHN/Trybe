6dce8bde002268baac5d3606e864a4032cfbfb8c
Alvaro Neto <alvaroshn@gmail.com>
2020-07-31 19:00:20 -0300

    parte 1

---
+++ exercicios/6_1/script.js
@@ -0,0 +1,11 @@
+let options = document.getElementById('state');
+let arrayEstados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
+
+for (let i=0 ; i < arrayEstados.length ; i += 1){
+    let option = document.createElement('option');
+    option.value=i;
+    option.innerText=arrayEstados[i];
+    options.appendChild(option);
+}
+
+
