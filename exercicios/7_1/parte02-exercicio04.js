9c2985948a40a893cc90049700011cbdc2d6bc52
Alvaro Neto <alvaroshn@gmail.com>
2020-08-11 22:06:28 -0300

    Exercicios bloco 07 dia 01

--- exercicios/7_1/parte02-exercicio04.js
+++ exercicios/7_1/parte02-exercicio04.js
@@ -2,3 +2,24 @@
 //Função 1: Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string.
 //Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
 //Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string.
+
+const globalArray = ["HTML", "CSS", "JavaScript", "React", "NodeJS"]
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} , aqui! Tudo bem?`
+    )
+    
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`
+
+    globalArray.forEach((skill, index) =>
+    result = `${result} - ${skill}`)
+    result = `${result}
+    #goTrybe`
+
+    return result
+}
+
+console.log(buildSkillsPhrase('Alvaro'));
