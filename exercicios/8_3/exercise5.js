11848e6866a86062f7fa8e9e8dbbd4edf84ad7b7
Alvaro Neto <alvaroshn@gmail.com>
2020-08-19 18:18:23 -0300

    Exercicios Bloco 08 dia 03

---
+++ exercicios/8_3/exercise5.js
@@ -0,0 +1,19 @@
+//Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  return names.reduce((acc,value) => acc + value.split('')
+  .reduce((acc, val) => (val === 'a'|| val === 'A') ? acc + 1 : acc ,0),0);
+}
+
+assert.deepEqual(containsA(), 20);
