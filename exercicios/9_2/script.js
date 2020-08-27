a31473a52f39e8ee1f73c9261c4f764044e53db2
Alvaro Neto <alvaroshn@gmail.com>
2020-08-27 19:47:47 -0300

    Exercicios Bloco 09 dia 02

--- exercicios/9_2/script.js
+++ exercicios/9_2/script.js
@@ -1,11 +1,18 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  fetch(API_URL, { headers: { Accept: 'application/json' } })
-    .then(response => response.json())
-    .then(data =>
-      document.getElementById('jokeContainer').innerText = data.joke
-    );
+const fetchJoke = async () => {
+  const response = await fetch(API_URL, { headers: { Accept: 'application/json' } })
+  const result = await response.json();
+  // console.log(response);
+  // console.log('---')
+  // console.log(result)
+  // console.log('****')
+  document.getElementById('jokeContainer').innerText = result.joke;
+  // fetch(API_URL, { headers: { Accept: 'application/json' } })
+  //   .then(response => response.json())
+  //   .then(data =>
+  //     document.getElementById('jokeContainer').innerText = data.joke
+  //   );
 }
 
 window.onload = () => fetchJoke();
