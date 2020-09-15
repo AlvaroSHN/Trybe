37b62b23b41de4ac2f9754795a516adda464ed33
Alvaro Neto <alvaroshn@gmail.com>
2020-09-15 19:20:30 -0300

    pokedex adicionado botao para exibir um pokemon por vez

--- exercicios/12_1/my-pokedex/src/Pokedex.js
+++ exercicios/12_1/my-pokedex/src/Pokedex.js
@@ -2,13 +2,29 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
-    }
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)  
+    this.state = { pokeNumber : 0 }    
+  }
+
+  handleClick = () => {
+    const pokemonLength = this.props.pokemons.length - 2;    
+    this.setState((prevStage, _props) => ({
+      pokeNumber : prevStage.pokeNumber + 1
+    }))
+    if (this.state.pokeNumber > pokemonLength) this.state.pokeNumber = 0
+  }
+
+  render() {
+    return (
+      <div className="pokedex">
+      {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
+        <Pokemon key={this.props.pokemons[this.state.pokeNumber].id} pokemon={this.props.pokemons[this.state.pokeNumber]} />
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
--- exercicios/12_1/my-pokedex/src/Pokemon.js
+++ exercicios/12_1/my-pokedex/src/Pokemon.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class Pokemon extends React.Component {
   render () {
     const { name, type, averageWeight, image } = this.props.pokemon;
-
+    
     return (
       <div className="pokemon">
         <div>
