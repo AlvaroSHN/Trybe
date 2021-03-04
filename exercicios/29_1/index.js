06cb4f78ba7c12559e4abb5e312bb2186499cbd7
Alvaro Neto <alvaroshn@gmail.com>
2021-03-04 15:33:05 -0300

    changing folder name

---
+++ exercicios/29_1/index.js
@@ -0,0 +1,14 @@
+// Utilizando Heroku
+const express = require("express");
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send("Trybe Hello!!");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+})
